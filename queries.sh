a75b9db1e5626560a1dd6d4c4c09defceeb75760
Livedata <info@livedata.it>
2025-06-24 16:04:56 +0200

    Add files via upload

--- queries.sh
+++ queries.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
 
@@ -8,88 +8,34 @@ echo -e "\nTotal number of goals in all games from winning teams:"
 echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
 
 echo -e "\nTotal number of goals in all games from both teams combined:"
-echo
+echo "$($PSQL "SELECT SUM(winner_goals + opponent_goals) FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams:"
-echo
+echo "$($PSQL "SELECT AVG(winner_goals) FROM games")"
 
 echo -e "\nAverage number of goals in all games from the winning teams rounded to two decimal places:"
-echo
+echo "$($PSQL "SELECT ROUND(AVG(winner_goals), 2) FROM games")"
 
 echo -e "\nAverage number of goals in all games from both teams:"
-echo
+echo "$($PSQL "SELECT AVG(winner_goals + opponent_goals) FROM games")"
 
 echo -e "\nMost goals scored in a single game by one team:"
-echo
+echo "$($PSQL "SELECT MAX(winner_goals) FROM games")"
 
 echo -e "\nNumber of games where the winning team scored more than two goals:"
-echo
+echo "$($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")"
 
 echo -e "\nWinner of the 2018 tournament team name:"
-echo
+echo "$($PSQL "SELECT name FROM teams WHERE team_id = (SELECT winner_id FROM games WHERE round = 'Final' AND year = 2018)")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo
+echo "$($PSQL "SELECT DISTINCT name FROM teams JOIN games ON teams.team_id = winner_id OR teams.team_id = opponent_id WHERE year = 2014 AND round = 'Eighth-Final' ORDER BY name")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo
+echo "$($PSQL "SELECT DISTINCT name FROM teams JOIN games ON teams.team_id = winner_id ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo
+echo "$($PSQL "SELECT year || '|' || name FROM games JOIN teams ON games.winner_id = teams.team_id WHERE round = 'Final' ORDER BY year")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo
-
-#!/bin/bash
-
-PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-
-# 1. Total number of goals in all games from winning teams
-echo $($PSQL "SELECT SUM(winner_goals) FROM games")
-
-# 2. Total number of goals in all games from both teams combined
-echo $($PSQL "SELECT SUM(winner_goals) + SUM(opponent_goals) FROM games")
-
-# 3. Average number of goals in all games from the winning teams
-echo $($PSQL "SELECT ROUND(AVG(winner_goals), 2) FROM games")
-
-# 4. Average number of goals in all games from both teams
-echo $($PSQL "SELECT ROUND(AVG(winner_goals + opponent_goals), 2) FROM games")
-
-# 5. Most goals scored in a single game by one team
-echo $($PSQL "SELECT MAX(GREATEST(winner_goals, opponent_goals)) FROM games")
-
-# 6. Number of games where the winning team scored more than two goals
-echo $($PSQL "SELECT COUNT(*) FROM games WHERE winner_goals > 2")
-
-# 7. Winner of the 2018 tournament team name
-echo $($PSQL "
-  SELECT t.name
-  FROM games g
-  JOIN teams t ON g.winner_id = t.team_id
-  WHERE g.round = 'Final' AND g.year = 2018
-")
-
-# 8. List of teams who played in the 2014 'Eighth-Final' round
-echo $($PSQL "
-  SELECT DISTINCT t.name
-  FROM games g
-  JOIN teams t ON t.team_id = g.winner_id OR t.team_id = g.opponent_id
-  WHERE g.round = 'Eighth-Final' AND g.year = 2014
-  ORDER BY t.name
-")
-
-# 9. List of unique winning team names in the whole data set
-echo $($PSQL "SELECT DISTINCT t.name FROM games g JOIN teams t ON g.winner_id = t.team_id ORDER BY t.name")
-
-# 10. Year and team name of all the champions
-echo $($PSQL "
-  SELECT g.year, t.name
-  FROM games g
-  JOIN teams t ON g.winner_id = t.team_id
-  WHERE g.round = 'Final'
-  ORDER BY g.year
-")
-
-# 11. List of teams that start with 'Co'
-echo $($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%' ORDER BY name")
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%'")"
